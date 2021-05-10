fca6057559961c4d188c6d25e37523667f7dba06
Emile <emilisbliudzius@gmail.com>
2021-05-10 18:29:56 +0100

    Added git prompt element

--- fish/config.fish
+++ fish/config.fish
@@ -2,4 +2,26 @@ setenv BROWSER "/usr/bin/firefox"
 alias reboot "sudo shutdown -r now"
 setenv TERM "xterm-256color"
 set fish_greeting ""
-alias emacs "emacs -nw"
+set -g __fish_git_prompt_show_informative_status 1
+set -g __fish_git_prompt_hide_untrackedfiles 1
+
+set -g __fish_git_prompt_color_branch -o blue
+set -g __fish_git_prompt_showupstream "informative"
+set -g __fish_git_prompt_char_upstream_ahead "↑"
+set -g __fish_git_prompt_char_upstream_behind "↓"
+set -g __fish_git_prompt_char_upstream_prefix ""
+
+set -g __fish_git_prompt_char_stagedstate "●"
+set -g __fish_git_prompt_char_dirtystate "✚"
+set -g __fish_git_prompt_char_untrackedfiles "…"
+set -g __fish_git_prompt_char_conflictedstate "✖"
+set -g __fish_git_prompt_char_cleanstate "✔"
+
+set -g __fish_git_prompt_color_dirtystate blue
+set -g __fish_git_prompt_color_stagedstate yellow
+set -g __fish_git_prompt_color_invalidstate red
+set -g __fish_git_prompt_color_untrackedfiles $fish_color_normal
+set -g __fish_git_prompt_color_cleanstate -o green
+
+set -g __fish_git_prompt_color_prefix -o blue
+set -g __fish_git_prompt_color_suffix -o blue
--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -8,6 +8,7 @@ function fish_prompt
     set_color cyan
     echo -n (prompt_pwd)
     set_color $fish_color_cwd
+    echo -n (fish_git_prompt)
     echo -n \)
     set_color normal
     echo -n ' > '
